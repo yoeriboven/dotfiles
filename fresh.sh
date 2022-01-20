f04debf4621484b9457dab8c60fa1ab62932526b
Yoeri Boven <yoeriboven@msn.com>
2022-01-20 20:31:06 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -35,13 +35,6 @@ $HOME/.composer/vendor/bin/valet install
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
 mkdir $HOME/Sites
 
-# Create sites subdirectories
-#mkdir $HOME/Sites/blade-ui-kit
-#mkdir $HOME/Sites/laravel
-
-# Clone Github repositories
-#./clone.sh
-
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 #rm -rf $HOME/.zshrc
 #ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
@@ -55,7 +48,7 @@ ln -s $HOME/.dotfiles/.phpcsfixer $HOME/.phpcsfixer
 #ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+#ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
