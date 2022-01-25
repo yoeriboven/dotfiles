3827f801195718743594db4f118dc10df7e8ec3c
Yoeri Boven <yoeriboven@msn.com>
2022-01-25 16:23:03 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -3,9 +3,9 @@
 echo "Setting up your Mac..."
 
 # Check for Oh My Zsh and install if we don't have it
-#if test ! $(which omz); then
-#  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
-#fi
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
